2799924f6aff68b51e01021421d487f54e09ace7
Valerian <valerian.purnomo@inticorp.tech>
2024-10-07 10:54:36 +0700

    Change title 2

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + Reactasdads</h1>
+      <h1>Test 1 2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
